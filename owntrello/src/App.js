382e73f128d97ca1eb0a3bdcc5ccf9c6463a3d6a
matiWaisman <83453393+matiWaisman@users.noreply.github.com>
2022-07-04 21:26:34 -0300

    Initial commit

---
+++ owntrello/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import PageFooter from "./components/pageFooter";
+
+function App() {
+  return (
+    <div className="App">
+      <PageFooter></PageFooter>
+    </div>
+  );
+}
+
+export default App;
